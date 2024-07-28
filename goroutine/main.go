1aed5b8d68cbbc064acd496a7370ec628e90d845
Htet Aung Khant <htetaung23.ha@gmail.com>
2024-07-28 17:37:06 +0630

    feat(app): learnt about channel(chan) and deadlock

--- goroutine/main.go
+++ goroutine/main.go
@@ -5,16 +5,22 @@ import (
 	"time"
 )
 
-func printText(text string) {
+func printText(text string, channel chan string) {
 	for i := 0; i < 5; i++ {
 		fmt.Println(text)
-		time.Sleep(800 * time.Millisecond)
+		time.Sleep(200 * time.Millisecond)
 	}
+	channel <- "Done"
 }
 
 func main() {
-	go printText("first")
-	go printText("second")
-	go printText("third")
-	time.Sleep(time.Minute)
+	// go printText("first")
+	// go printText("second")
+	// go printText("third")
+	// time.Sleep(time.Minute)
+
+	var channel chan string // channel := make(chan string)
+	go printText("Passing channel with thread", channel)
+	response := <-channel
+	fmt.Println(response)
 }
