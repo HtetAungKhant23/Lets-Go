572273a1668c0dcd625df19d5961d6e0e399c313
Htet Aung Khant <htetaung23.ha@gmail.com>
2024-07-29 13:48:24 +0630

    feat(app): learnt about wait group from sync package for async await feature using closure function

--- cryptomaster/main.go
+++ cryptomaster/main.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/htetaungkhant/go/crypto/api"
 )
 
 func main() {
-	res, err := api.GetRate("btc")
+	currencies := []string{"btc", "eth", "sol", "bch"}
+	var wg sync.WaitGroup // using wait group for async await feature
+	for _, currency := range currencies {
+		wg.Add(1)
+		go func(currency string) { // closure function
+			getCurrencyRate(currency)
+			wg.Done()
+		}(currency)
+	}
+	wg.Wait()
+}
+
+func getCurrencyRate(currency string) {
+	res, err := api.GetRate(currency)
 	if err == nil {
 		fmt.Printf("This rate for %v is %.2f \n", res.Currency, res.Price)
 	}
