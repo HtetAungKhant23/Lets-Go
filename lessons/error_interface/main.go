26e169124ff64971ae2a764d2bdbe45cc7023708
Htet Aung Khant <htetaung23.ha@gmail.com>
2024-09-29 20:35:46 +0630

    feat(error): error formatted string

--- lessons/error_interface/main.go
+++ lessons/error_interface/main.go
@@ -11,16 +11,20 @@ func sendSMSToCouple(msgToCustomer, msgToSpouse string) (int, error) {
 	return costForCus + costForSpouse, nil
 }
 
+func getSMSErrorString(cost float64, recipient string) string {
+	return fmt.Sprintf("SMS that costs %v to be sent to %s cannot be sent", cost, recipient)
+}
+
 func sendSMS(message string) (int, error) {
 	const maxTextLen = 25
 	const costPerChar = 2
 	if len(message) > maxTextLen {
-		return 0, fmt.Errorf("can't send texts over %v characters", maxTextLen)
+		return 0, fmt.Errorf(getSMSErrorString(float64(costPerChar*len(message)), "09123456789"))
 	}
 	return costPerChar * len(message), nil
 }
 
 func main() {
-	totalCost, err := sendSMSToCouple("Good Shal", "Shal Good kwar")
+	totalCost, err := sendSMSToCouple("Good Shal asdfa asdfqe2 aswer asdzcarg asdfrqsf", "Shal Good kwar")
 	fmt.Println(totalCost, err)
 }
