24de31efce5c22298eb2eb7ecc2e08ff63715c4f
Htet Aung Khant <htetaung23.ha@gmail.com>
2024-09-30 17:24:51 +0630

    feat(slice): learn about slice and exercise

--- lessons/slice/main.go
+++ lessons/slice/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func getMessageWithRetries(primary, secondary, tertiary string) ([3]string, [3]int) {
 	str := [3]string{primary, secondary, tertiary}
@@ -13,12 +16,36 @@ func getMessageWithRetries(primary, secondary, tertiary string) ([3]string, [3]i
 	return str, costs
 }
 
+const (
+	planFree = "free"
+	planPro  = "pro"
+)
+
+func getMessageWithRetriesForPlan(plan string, messages [3]string) ([]string, error) {
+	switch plan {
+	case planFree:
+		return messages[:2], nil
+	case planPro:
+		return messages[:], nil
+	default:
+		return nil, errors.New("unsupported plan")
+	}
+}
+
 func main() {
-	pri := "Hello sir/madam can I interest you in a yacht?"
-	sec := "Please I'll even give you an Amazon gift card?"
-	ter := "You're missing out big time"
+	//pri := "Hello sir/madam can I interest you in a yacht?"
+	//sec := "Please I'll even give you an Amazon gift card?"
+	//ter := "You're missing out big time"
+	//
+	//strArr, costArr := getMessageWithRetries(pri, sec, ter)
+	//
+	//fmt.Println(strArr, costArr)
 
-	strArr, costArr := getMessageWithRetries(pri, sec, ter)
+	slice, err := getMessageWithRetriesForPlan("", [3]string{
+		"Hello sir/madam can I interest you in a yacht?",
+		"Please I'll even give you an Amazon gift card?",
+		"You're missing out big time",
+	})
 
-	fmt.Println(strArr, costArr)
+	fmt.Println(slice, err)
 }
