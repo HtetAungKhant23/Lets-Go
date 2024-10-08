2f88c4f84840a66ef905aa1ee449297cd26332bb
Htet Aung Khant <htetaung23.ha@gmail.com>
2024-10-08 16:08:03 +0630

    feat(map): count distinct word exercise

--- lessons/map/exercise/exe3/main.go
+++ lessons/map/exercise/exe3/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func getNameCounts(names []string) map[rune]map[string]int {
 	res := make(map[rune]map[string]int)
@@ -15,7 +18,38 @@ func getNameCounts(names []string) map[rune]map[string]int {
 	return res
 }
 
+func countDistinctWords(messages []string) int {
+	counter := 0
+	counterMap := make(map[string]int)
+	for _, msg := range messages {
+		splitMsg := strings.Split(msg, " ")
+		for _, m := range splitMsg {
+			m = strings.ToLower(m)
+			_, ok := counterMap[m]
+			if !ok {
+				counterMap[m] = 1
+				counter++
+			}
+		}
+	}
+	return counter
+}
+
 func main() {
-	res := getNameCounts([]string{"😊", "htet", "koHAK", "khant", "htoo", "koHAK"})
-	fmt.Println(res)
+	//res := getNameCounts([]string{"😊", "htet", "koHAK", "khant", "htoo", "koHAK"})
+	//fmt.Println(res)
+
+	count := countDistinctWords([]string{
+		"LF9M UBRS need all",
+		"LF8M UBRS need all",
+		"LF7M UBRS need all",
+		"LF6M UBRS need tanks and heals",
+		"LF5M UBRS need tanks and heals",
+		"LF4M UBRS need tanks and heals",
+		"LF3M UBRS need tanks and healer",
+		"LF2M UBRS need tanks",
+		"LF1M UBRS need tank",
+		"Group is full thanks!",
+	})
+	fmt.Println(count)
 }
