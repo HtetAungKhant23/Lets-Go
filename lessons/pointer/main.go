a470d1e60034ea0c80fe78d6099daeedf12679ee
Htet Aung Khant <htetaung23.ha@gmail.com>
2024-10-10 10:55:15 +0630

    feat(pointer): exercise

---
+++ lessons/pointer/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+type Message struct {
+	Recipient string
+	Text      string
+}
+
+func getMessageText(m Message) string {
+	return fmt.Sprintf(`
+To: %v
+Message: %v
+`, m.Recipient, m.Text)
+}
+
+func removeProfanity(message *string) {
+	replaceWord := map[string]string{
+		"fubb":  "****",
+		"shiz":  "****",
+		"witch": "*****",
+	}
+	fmt.Println("before ", message, *message)
+	for k, word := range replaceWord {
+		*message = strings.ReplaceAll(*message, k, word)
+	}
+	fmt.Println("after ", message, *message)
+}
+
+func main() {
+	s := "Oh man I've seen some crazy ass shiz in my time..."
+	removeProfanity(&s)
+
+	msg := Message{"asdf", "jk;lk"}
+	fmt.Println(getMessageText(msg))
+}
